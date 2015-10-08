ba93f32c09af309cc61a9a067c7e69d5d8f6d1e9
Richard Martyn <dickmartyn@gmail.com>
2015-10-07 23:13:15 -0700

    Buttons coded, no operators or memory though.

--- app/src/main/java/com/example/t00040181/calculator/MainActivity.java
+++ app/src/main/java/com/example/t00040181/calculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.t00040181.calculator;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,66 +14,79 @@ import android.graphics.PorterDuff;
 public class MainActivity extends AppCompatActivity {
 
     TextView calc_input, mem_buffer, calc_history;
+    String default_input = "0";
 
+    private Toast msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.b_0).setOnClickListener(buttonListener);
-        findViewById(R.id.b_1).setOnClickListener(buttonListener);
-        findViewById(R.id.b_2).setOnClickListener(buttonListener);
-        findViewById(R.id.b_3).setOnClickListener(buttonListener);
-        findViewById(R.id.b_4).setOnClickListener(buttonListener);
-        findViewById(R.id.b_5).setOnClickListener(buttonListener);
-        findViewById(R.id.b_6).setOnClickListener(buttonListener);
-        findViewById(R.id.b_7).setOnClickListener(buttonListener);
-        findViewById(R.id.b_8).setOnClickListener(buttonListener);
-        findViewById(R.id.b_9).setOnClickListener(buttonListener);
-        findViewById(R.id.b_C).setOnClickListener(buttonListener);
-        findViewById(R.id.b_CE).setOnClickListener(buttonListener);
-        findViewById(R.id.b_Add).setOnClickListener(buttonListener);
-        findViewById(R.id.b_Sub).setOnClickListener(buttonListener);
-        findViewById(R.id.b_Div).setOnClickListener(buttonListener);
-        findViewById(R.id.b_Mul).setOnClickListener(buttonListener);
-        findViewById(R.id.b_Dec).setOnClickListener(buttonListener);
-        findViewById(R.id.b_Equal).setOnClickListener(buttonListener);
-        findViewById(R.id.b_MC).setOnClickListener(buttonListener);
-        findViewById(R.id.b_Madd).setOnClickListener(buttonListener);
-        findViewById(R.id.b_Msub).setOnClickListener(buttonListener);
-        findViewById(R.id.b_MR).setOnClickListener(buttonListener);
-        findViewById(R.id.b_Back).setOnClickListener(buttonListener);
-        findViewById(R.id.b_Sign).setOnClickListener(buttonListener);
+        showButtonPress(findViewById(R.id.b_0));
+        showButtonPress(findViewById(R.id.b_1));
+        showButtonPress(findViewById(R.id.b_2));
+        showButtonPress(findViewById(R.id.b_3));
+        showButtonPress(findViewById(R.id.b_4));
+        showButtonPress(findViewById(R.id.b_5));
+        showButtonPress(findViewById(R.id.b_6));
+        showButtonPress(findViewById(R.id.b_7));
+        showButtonPress(findViewById(R.id.b_8));
+        showButtonPress(findViewById(R.id.b_9));
+        showButtonPress(findViewById(R.id.b_C));
+        showButtonPress(findViewById(R.id.b_CE));
+        showButtonPress(findViewById(R.id.b_Add));
+        showButtonPress(findViewById(R.id.b_Sub));
+        showButtonPress(findViewById(R.id.b_Div));
+        showButtonPress(findViewById(R.id.b_Mul));
+        showButtonPress(findViewById(R.id.b_Dec));
+        showButtonPress(findViewById(R.id.b_Equal));
+        showButtonPress(findViewById(R.id.b_MC));
+        showButtonPress(findViewById(R.id.b_Madd));
+        showButtonPress(findViewById(R.id.b_Msub));
+        showButtonPress(findViewById(R.id.b_MR));
+        showButtonPress(findViewById(R.id.b_Back));
+        showButtonPress(findViewById(R.id.b_Sign));
+
+        calc_input = (TextView) findViewById(R.id.calc_input);
     }
     private View.OnClickListener buttonListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Button b = (Button)v;
-            calc_input = (TextView) findViewById(R.id.calc_input);
-            String current_input = calc_input.getText().toString();
-            String added_input = "";
+            showButtonPress(b);
+            String c = calc_input.getText().toString();
+            String a = b.getText().toString();
+
             switch(b.getId()){
                 case R.id.b_0:
-                    added_input+=b.getTag().toString();
-                    //calc_input.setText(current_input+added_input);
+                    inputHandler(b, c, a);
                     break;
                 case R.id.b_1:
+                    inputHandler(b, c, a);
                     break;
                 case R.id.b_2:
+                    inputHandler(b, c, a);
                     break;
                 case R.id.b_3:
+                    inputHandler(b, c, a);
                     break;
                 case R.id.b_4:
+                    inputHandler(b, c, a);
                     break;
                 case R.id.b_5:
+                    inputHandler(b, c, a);
                     break;
                 case R.id.b_6:
+                    inputHandler(b, c, a);
                     break;
                 case R.id.b_7:
+                    inputHandler(b, c, a);
                     break;
                 case R.id.b_8:
+                    inputHandler(b, c, a);
                     break;
                 case R.id.b_9:
+                    inputHandler(b, c, a);
                     break;
                 case R.id.b_Div:
                     break;
@@ -91,30 +105,76 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.b_MR:
                     break;
                 case R.id.b_Dec:
+                    if(c.indexOf('.') >= 0){
+                        break;
+                    } else if(c.length() >= 15){
+                        break;
+                    } else {
+                        calc_input.setText(c + a);
+                    }
                     break;
                 case R.id.b_Equal:
                     break;
                 case R.id.b_Back:
+                    if(c.length() == 1) {
+                        calc_input.setText(default_input);
+                        break;
+                    } else {
+                        if(c.length() > 1){
+                            c = c.substring(0, c.length()-1);
+                            calc_input.setText(c);
+                        }
+                    }
                     break;
                 case R.id.b_C:
                     break;
                 case R.id.b_CE:
+                    calc_input.setText(default_input);
                     break;
                 case R.id.b_Sign:
+                    if(c.indexOf('-') >= 0){
+                        calc_input.setText(c.substring(1));
+                    } else if(c.equals(default_input)){
+                        break;
+                    } else {
+                        calc_input.setText('-'+c);
+                    }
                     break;
             }
-
         }
     };
-    /*private class buttonListener implements View.OnClickListener {
-        public void onClick(View v){
 
+    public void inputHandler (Button b, String current_input, String added_input){
+        if(current_input.equals(default_input)) {
+            calc_input.setText(added_input);
+        } else {
+            if((current_input+added_input).length() > 15){
+                /* The following if is a "toast hack" to limit toast spam for input overflow.
+                    credit: S.A. Jay, stackoverflow
+                    http://stackoverflow.com/questions/2755277/android-hide-all-showed-toast-messages
+                 */
+                if(null == msg) {
+                    msg = Toast.makeText(getApplicationContext(), "Input limit exceeded.", Toast.LENGTH_SHORT);
+                    msg.show();
+
+                    new Handler().postDelayed(new Runnable() {
+                        public void run() {
+                            msg = null;
+                        }
+                    }, 2000);
+                }
+            } else {
+                calc_input.setText(current_input + added_input);
+            }
         }
-    }*/
+    }
 
-
-
-    public static void showButtonPress(View button){
+    /* Method to display button presses.
+        Credit: András at stackoverflow
+        http://stackoverflow.com/questions/7175873/click-effect-on-button-in-android
+     */
+    public void showButtonPress(View button){
+        button.setOnClickListener(buttonListener);
         button.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -134,6 +194,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
